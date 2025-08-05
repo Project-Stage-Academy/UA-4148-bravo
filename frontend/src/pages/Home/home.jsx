a567f6804f58b5905bca81ea5f602acc2003acb8
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-08-05 23:28:25 +0200

    Hero section

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,4 +1,5 @@
 import './home.css';
+import Button from '../../components/Button/button';
 
 /**
  * HomePage component
@@ -7,7 +8,42 @@ import './home.css';
  */
 function HomePage() {
     return (
-        <></>
+        <div className={'home'}>
+            <section className={'home-section home-section__green hero-section'}>
+                <div className={'hero-title'}>
+                    <h2 className={'home-title-1 hero-title__title-1'}>CRAFTMERGE</h2>
+                    <h2 className={'home-title-2 hero-title__title-2'}>Обʼєднуємо крафтових виробників та інноваторів</h2>
+                    <div>
+                        <Button className={'button__padding'}>
+                            Детальніше про нас
+                        </Button>
+                    </div>
+                </div>
+                <div className={'hero-pictures'}>
+                    <picture>
+                        <source media="(max-width: 646px)" srcSet="/pictures/png/hero-small.png" />
+                        <source media="(max-width: 1481px)" srcSet="/pictures/png/hero-middle.png" />
+                        <img src={'/pictures/png/hero-big.png'} alt={'Hero'}/>
+                    </picture>
+                </div>
+            </section>
+
+            <section className={'home-section home-section__white'}>
+
+            </section>
+
+            <section className={'home-section home-section__green'}>
+
+            </section>
+
+            <section className={'home-section home-section__yellow'}>
+
+            </section>
+
+            <section className={'home-section home-section__white'}>
+
+            </section>
+        </div>
     );
 }
 
