a97a298d5ac0176ed53d18afe0fcc26c56f36c53
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-08-12 16:44:29 +0200

    Refactor Who We Are component styles and class names for consistency

--- frontend/src/pages/WhoWeAre/whoWeAre.jsx
+++ frontend/src/pages/WhoWeAre/whoWeAre.jsx
@@ -12,13 +12,13 @@ import Image from '../../components/Image/image';
  */
 function WhoWeAre() {
     return (
-        <div className={'wwa'}>
-            <div className={'wwa--container'}>
-                <h2 className={'wwa--title'}>Хто ми</h2>
-                <div className={'wwa--content-container'}>
-                    <div className={'wwa--content-text'}>
+        <div className={'who-we-are'}>
+            <div className={'who-we-are__container'}>
+                <h2 className={'who-we-are__title'}>Хто ми</h2>
+                <div className={'who-we-are__content'}>
+                    <div className={'who-we-are__text'}>
                         <p>
-                            <span className={'wwa--content-text__bold'}>CraftMerge</span> - перший форум Західної України, який створений у співпраці з Національним університетом «Львівська політехніка». Наша місія - не лише об'єднання українських виробників та стартапів, а й відкриття нових перспектив у виробничій галузі.
+                            <span className={'who-we-are__text--bold'}>CraftMerge</span> - перший форум Західної України, який створений у співпраці з Національним університетом «Львівська політехніка». Наша місія - не лише об'єднання українських виробників та стартапів, а й відкриття нових перспектив у виробничій галузі.
                         </p>
                         <p>
                             CraftMerge - це не лише платформа для обміну досвідом та ідеями, але й комунікаційний майданчик для обговорення актуальних тенденцій та передових технологій.
@@ -27,8 +27,8 @@ function WhoWeAre() {
                             Учасники форуму отримають можливість не лише обмінятися досвідом та ідеями, але й ознайомитися з найсучаснішими рішеннями виробництва крафтової продукції. Ми створили CraftMerge, щоб допомогти українським виробникам збільшити свою популярність та впізнаваність, розширити аудиторію споживачів та залучити нових клієнтів. Приєднуйтеся до нашого форуму та розвивайте свій бізнес разом з нами!"
                         </p>
                     </div>
-                    <div className={'wwa--content-image'}>
-                        <Image src={'/pictures/png/pexels-pavel-danilyuk.png'} alt={'Who we are'} className={'wwa--image'} />
+                    <div className={'who-we-are__image-container'}>
+                        <Image src={'/pictures/png/pexels-pavel-danilyuk.png'} alt={'Who we are'} className={'who-we-are__image'} />
                     </div>
                 </div>
             </div>
