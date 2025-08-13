20fc89d8b3132e6eff795c4ed052fe88b8721e12
Anastasiia Veselovska <veselovskaaanastasia03@gmail.com>
2025-08-14 00:43:22 +0300

    Resolve merge conflict

--- tests/users/__init__.py
+++ tests/users/__init__.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import unittest
-
-def load_tests(loader, tests, pattern):
-    return loader.discover(start_dir=__name__, pattern='test_*.py')
-=======
 import os
 import unittest
 
 def load_tests(loader, tests, pattern):
     this_dir = os.path.dirname(os.path.abspath(__file__))
     return loader.discover(start_dir=this_dir, pattern='test_*.py')
->>>>>>> 29354e87ea4e1fe07cbab918b932b5ee970e5775
