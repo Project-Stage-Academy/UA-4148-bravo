201b91313920c95c2263e2e35a7c0992aa295dc5
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-08-15 15:56:30 +0200

    Add custom password recovery and authentication endpoints

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,25 @@ from .views import CustomTokenObtainPairView
 from .views import CustomPasswordResetView, CustomPasswordResetConfirmView
 
 urlpatterns = [
+    # ----------------------------------------
+    # Custom password recovery endpoints
+    # ----------------------------------------
+    path('reset_password/', CustomPasswordResetView.as_view(), name="custom_reset_password"),
+    path('reset_password_confirm/', CustomPasswordResetConfirmView.as_view(), name="custom_reset_password_confirm"),
 
+    # ----------------------------------------
+    # Custom authentication endpoint
+    # ----------------------------------------
+    path('login/', CustomTokenObtainPairView.as_view(), name='custom_login'),
+
+    # ----------------------------------------
+    # Djoser authentication endpoints
+    # ----------------------------------------
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+
+    # ----------------------------------------
+    # JWT Logout endpoint
+    # ----------------------------------------
+    path('auth/jwt/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
