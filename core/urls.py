de3cb2f42c625b8ba86b7e0022e007808c454771
vanichka <138333895+MumuCarrot@users.noreply.github.com>
2025-08-15 15:55:57 +0200

    Refactor authentication URLs and add custom OAuth endpoint

--- core/urls.py
+++ core/urls.py
@@ -21,68 +21,17 @@ api_urlpatterns = [
     # User authentication endpoints
     path('users/', include('users.urls')),  # User-specific endpoints
 
-    # Register
-    # URL: /api/v1/auth/register/
-    # Req: { email, first_name, last_name, password, password2 }
-    # Res: 201 { status, message, user_id, email }
-
+    # Authentication endpoints
     path('auth/register/', UserRegistrationView.as_view(), name='user_register'),
-
-    # Resend register email
-    # URL: /api/v1/auth/register/resend/
-    # Req: { email, userId }
-    # Res: 201 { status, message, user_id, email }
-
-    # path('auth/register/resend', , name='user_register'),
-
-    # Me
-    # URL: /api/v1/auth/me/
-    # Req: {}
-    # Res: 200 { id, email, role, ... }
-
-    # path('auth/me/', CustomTokenObtainPairView.as_view(), name='custom_login'),
-
-    # Password reset
-    # URL: /api/v1/auth/password/reset/
-    # Req: { email }
-    # Res: 200
-
-    path('auth/password/reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-
-    # Password reset confirm
-    # URL: /api/v1/auth/password/reset/confirm/
-    # Req: { uid, token, new_password }
-    # Res: 200
-
-    path('auth/password/reset/confirm/',
-         CustomPasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-
-    # Create
-    # URL: /api/v1/auth/jwt/create/
-    # Req: { email, password }
-    # Res: 200 { access, refresh }
-
     path('auth/jwt/create/', CustomTokenObtainPairView.as_view(), name='jwt-create'),
-
-    # Refresh
-    # URL: /api/v1/auth/jwt/refresh/
-    # Req: { refresh }
-    # Res: 200 { access }
-
     path('auth/jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
-
-    # Blacklist
-    # URL: /api/v1/auth/jwt/blacklist/
-    # Req: { refresh }
-    # Res: 205
-
-    path('auth/jwt/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-
-    # Email verification
+    path('auth/jwt/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('auth/verify-email/<int:user_id>/<str:token>/',
-        VerifyEmailView.as_view(), 
+        VerifyEmailView.as_view(),
         name='verify-email'),
+    # Custom OAuth endpoint
+    path('oauth/login/', OAuthTokenObtainPairView.as_view(), name="oauth_login"),
+
 
     # Project-related endpoints
     path('projects/', include('projects.urls')),
@@ -92,9 +41,6 @@ api_urlpatterns = [
 
     # Investor-related endpoints
     path('investors/', include('investors.urls')),
-
-    # OAuth endpoints
-    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns = [
