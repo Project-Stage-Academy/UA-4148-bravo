43b38041a7d5550284a51fcf61de1e4ecfb25fe3
Olena Ivashchenko <dvd7776@gmail.com>
2025-08-15 18:37:12 +0300

    created models for notification preferences

--- core/urls.py
+++ core/urls.py
@@ -31,8 +31,10 @@ api_urlpatterns = [
         name='verify-email'),
     # Custom OAuth endpoint
     path('oauth/login/', OAuthTokenObtainPairView.as_view(), name="oauth_login"),    
-    
 
+    # Communications endpoints
+    path('communications/', include('communications.urls')),
+    
     # Project-related endpoints
     path('projects/', include('projects.urls')),
 
