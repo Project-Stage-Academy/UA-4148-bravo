3b4e560203036d313c22852f3a085cacd7fb626a
“Anton <shulezhko.antony@gmail.com>
2025-08-24 11:17:52 +0300

    new assertion in tests, which checks for the entire, exact error string

--- tests/startups/test_api_create.py
+++ tests/startups/test_api_create.py
@@ -104,14 +104,29 @@ class StartupCreateAPITests(BaseCompanyCreateAPITestCase):
         Ensure creating a startup with an invalid 'stage' value fails validation.
         """
         payload = self.get_valid_payload()
-        payload["stage"] = "invalid_stage_value"
+        invalid_stage_value = "invalid_stage_value"
+        payload["stage"] = invalid_stage_value
         response = self.client.post(self.url, payload, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("stage", response.data)
         
-        self.assertIn("is not a valid choice", str(response.data['stage']))
-        self.assertIn("greater than or equal to", str(response.data["fund_size"]))
+        expected_error_message = f'"{invalid_stage_value}" is not a valid choice.'
+        error_messages = [str(e) for e in response.data['stage']]
+        self.assertIn(expected_error_message, error_messages)
+
+    def test_creation_with_invalid_fund_size_fails(self):
+        """
+        Ensure creating an investor with a negative fund size fails validation.
+        """
+        payload = self.get_valid_payload()
+        payload["fund_size"] = "-1000.00"
+        response = self.client.post(self.url, payload, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("fund_size", response.data)
+
+        self.assertIn("Ensure this value is greater than or equal to 0.", str(response.data["fund_size"]))
 
     def test_creation_missing_required_fields_fails(self):
         """
